07d0620ef0e7cff772b15d3a403525643037b5ba
rahulsharma09 <08rahul.sharma01@gmail.com>
2021-09-30 08:43:50 +0530

    update-language

--- js/service.js
+++ js/service.js
@@ -13,6 +13,23 @@ function langaugeEnglish() {
     "Logiio provides end-to-end delivery services. It helps to save time and hassle that comes with the parcel delivery of logistics. It has a tracking system that monitors the whereabouts of each parcel at all times according to your preferences, including orders and other related details. There is also an order management system for users who would like to track their orders.";
   document.getElementById("do-p2").innerHTML =
     "Logiio helps in providing the best service by organizing and managing the whole unorganized sector of logistics. You just have to enter the weight, source, and destination of the parcel and it will automatically calculate the cost for your service.";
+
+    // Our Motto
+    document.getElementById("motto-title").innerHTML="Our Motto"
+    document.getElementById("motto-1").innerHTML="To provide undisturbed easy and untroubled delivery services";
+    document.getElementById("motto-2").innerHTML="Live tracking for all orders and providing the best services at a reasonable cost.";
+    document.getElementById("motto-3").innerHTML=" It’ll store all previous delivery history and other order related details, all under one roof";
+    document.getElementById("motto-4").innerHTML="To save your time and effort."
+    document.getElementById("motto-5").innerHTML="We provide an end-to-end delivery - from your entered source and destination."
+    document.getElementById("motto-6").innerHTML="With the effortless and smooth features of Logiio, you would be able to reduce paperwork and have a pleasant experience."
+    document.getElementById("motto-7").innerHTML="To make a mobile application that is simple and easy to use."
+
+
+    // Details
+    document.getElementById("detail-1").innerHTML = "Logiio allows you to store and access all your past order details from anywhere, anytime. In simple words, Logiio is a mobile application that gives you access to all your order-related information, anytime and anywhere.";
+    document.getElementById("detail-2").innerHTML = "It helps you organize and manage all your details in a single place. It's easy to access and you can also track your orders on the go with this dedicated application.";
+    document.getElementById("detail-3").innerHTML = "These days as per the situation of the Covid pandemic, it’s very difficult to find the best logistics service providers, Logiio solves these problems for you. ";
+    document.getElementById("detail-4").innerHTML = "If you are looking for the best logistics services then Logiio is your friend, indeed!"
 }
 
 function languageHindi() {
@@ -31,10 +48,27 @@ function languageHindi() {
     "लॉजियो एंड-टू-एंड डिलीवरी सेवाएं प्रदान करता है। यह आने वाले समय और परेशानी को बचाने में मदद करता है।इसमें एक ट्रैकिंग सिस्टम है जो प्रत्येक के ठिकाने पर नज़र रखता है  ऑर्डर और अन्य संबंधित विवरणों सहित आपकी प्राथमिकताओं के अनुसार हर समय पार्सल। उन उपयोगकर्ताओं के लिए एक आदेश प्रबंधन प्रणाली भी है जो अपने आदेशों को ट्रैक करना चाहते हैं।";
   document.getElementById("do-p2").innerHTML =
     "लॉजियो रसद के पूरे असंगठित क्षेत्र को व्यवस्थित और प्रबंधित करके सर्वोत्तम सेवा प्रदान करने में मदद करता है। आपको बस पार्सल का वजन, स्रोत और गंतव्य दर्ज करना होगा और यह स्वचालित रूप से आपकी सेवा की लागत की गणना करेगा।";
+
+    // Our Motto
+    document.getElementById("motto-title").innerHTML="हमारे आदर्श वाक्य"
+    document.getElementById("motto-1").innerHTML="निर्बाध आसान और निर्बाध वितरण सेवाएं प्रदान करने के लिए";
+    document.getElementById("motto-2").innerHTML="सभी ऑर्डर के लिए लाइव ट्रैकिंग और उचित कीमत पर सर्वोत्तम सेवाएं प्रदान करना।";
+    document.getElementById("motto-3").innerHTML="यह सभी पिछले डिलीवरी इतिहास और ऑर्डर से संबंधित अन्य विवरणों को एक ही छत के नीचे संग्रहीत करेगा";
+    document.getElementById("motto-4").innerHTML="अपना समय और प्रयास बचाने के लिए।"
+    document.getElementById("motto-5").innerHTML="हम आपके द्वारा दर्ज किए गए स्रोत और गंतव्य से - एंड-टू-एंड डिलीवरी प्रदान करते हैं।"
+    document.getElementById("motto-6").innerHTML="लॉजियो की सहज और सहज सुविधाओं के साथ, आप कागजी कार्रवाई को कम करने और सुखद अनुभव प्राप्त करने में सक्षम होंगे।"
+    document.getElementById("motto-7").innerHTML="एक ऐसा मोबाइल एप्लिकेशन बनाना जो सरल और उपयोग में आसान हो।"
+
+
+    // Details
+    document.getElementById("detail-1").innerHTML = "लॉजियो आपको कहीं से भी, कभी भी अपने सभी पिछले ऑर्डर विवरणों को स्टोर और एक्सेस करने की अनुमति देता है। सरल शब्दों में, लॉजियो एक मोबाइल एप्लिकेशन है जो आपको किसी भी समय और कहीं भी, आपके ऑर्डर से संबंधित सभी जानकारी तक पहुंच प्रदान करता है।";
+    document.getElementById("detail-2").innerHTML = "यह आपके सभी विवरणों को एक ही स्थान पर व्यवस्थित और प्रबंधित करने में आपकी सहायता करता है। इसे एक्सेस करना आसान है और आप इस समर्पित एप्लिकेशन के साथ चलते-फिरते अपने ऑर्डर ट्रैक भी कर सकते हैं।";
+    document.getElementById("detail-3").innerHTML = "इन दिनों कोविड महामारी की स्थिति के अनुसार, सर्वश्रेष्ठ लॉजिस्टिक्स सेवा प्रदाताओं को खोजना बहुत मुश्किल है, लॉजियो आपके लिए इन समस्याओं का समाधान करता है।";
+    document.getElementById("detail-4").innerHTML = "यदि आप सर्वश्रेष्ठ लॉजिस्टिक्स सेवाओं की तलाश में हैं तो लॉजियो वास्तव में आपका मित्र है!"
 }
 
 function languageMarathi() {
-  document.getElementById("solve-title").innerHTML = "लॉगिओ काय सोडवतो?";
+  document.getElementById("solve-title").innerHTML = "लॉजियो काय सोडवतो?";
   document.getElementById("loggio-p1").innerHTML =
     "लॉगिओ हे एक मोबाइल अनुप्रयोग आहे जे विशेषत: आपल्यासाठी अद्वितीय उपाय आणण्यासाठी डिझाइन केले गेले आहे आण आदेशांची पूर्तता, उत्तम गुणवत्ता, वेग आणि वाजवी किंमत याची खात्री.  ते बांधले गेले आहे एंड-टू-एंड लॉजिस्टिक्स लक्षात ठेवून आणि सर्वांसाठी विविध अद्वितीय उपाय प्रदान करण्याचे उद्दीष्ट आहे पिकअप आणि डिलिव्हरीच्या योग्य गरजा. कुरिअरचे जे आपले जीवन सोपे करते आणि बनवते ऑन-द-गो ऑर्डर्स आणि पार्सल वितरित करणे सोपे आहे.";
   document.getElementById("loggio-p2").innerHTML =
@@ -48,4 +82,20 @@ function languageMarathi() {
     "लॉजियो एंड-टू-एंड डिलीव्हरी सेवा प्रदान करते. हे वेळ वाचवण्यास मदत करते आणि रसद वितरणाची अडचण. यात एक ट्रॅकिंग सिस्टम आहे जी प्रत्येकाच्या ठावठिकाणी निरीक्षण करते ऑर्डर आणि इतर संबंधित तपशीलांसह, आपल्या आवडीनुसार प्रत्येक वेळी पार्सल करा. वापरकर्त्यांसाठी ऑर्डर व्यवस्थापन प्रणाली देखील आहे जी त्यांच्या ऑर्डरचा मागोवा घेऊ इच्छित आहेत. ";
   document.getElementById("do-p2").innerHTML =
     "लॉजियो संपूर्ण असंघटित संघटित आणि व्यवस्थापित करून सर्वोत्तम सेवा प्रदान करण्यात मदत करते रसद क्षेत्र. आपल्याला फक्त पार्सलचे वजन, स्त्रोत आणि गंतव्य प्रविष्ट करावे लागेल तुमच्या सेवेच्या खर्चाची आपोआप गणना होईल.";
+
+    // Our Motto
+    document.getElementById("motto-title").innerHTML="आमचे बोधवाक्य"
+    document.getElementById("motto-1").innerHTML="निर्विघ्न सुलभ आणि अस्वस्थ वितरण सेवा प्रदान करणे";
+    document.getElementById("motto-2").innerHTML="सर्व ऑर्डरसाठी थेट ट्रॅकिंग आणि वाजवी किंमतीत सर्वोत्तम सेवा प्रदान करणे.";
+    document.getElementById("motto-3").innerHTML="हे मागील सर्व वितरण इतिहास आणि ऑर्डरशी संबंधित इतर तपशील, सर्व एकाच छताखाली संग्रहित करेल";
+    document.getElementById("motto-4").innerHTML="आपला वेळ आणि मेहनत वाचवण्यासाठी."
+    document.getElementById("motto-5").innerHTML="आम्ही एंटर-टू-एंड डिलिव्हरी प्रदान करतो-आपल्या प्रविष्ट केलेल्या स्त्रोत आणि गंतव्यस्थानावरून."
+    document.getElementById("motto-6").innerHTML="लॉजियो च्या सहज आणि गुळगुळीत वैशिष्ट्यांसह, आपण पेपरवर्क कमी करण्यास आणि एक सुखद अनुभव घेण्यास सक्षम असाल."
+    document.getElementById("motto-7").innerHTML="मोबाइल अनुप्रयोग बनवणे जे सोपे आणि वापरण्यास सुलभ आहे."
+
+    // Details
+    document.getElementById("detail-1").innerHTML = "लॉजियो आपल्याला आपल्या सर्व मागच्या ऑर्डरचे तपशील कोठूनही, कधीही संग्रहित करण्याची आणि प्रवेश करण्याची परवानगी देते. मध्ये सोप्या शब्दात, Logiio हा एक मोबाईल एप्लिकेशन आहे जो तुम्हाला तुमच्या सर्व ऑर्डरशी संबंधित प्रवेश देतो माहिती, कधीही आणि कुठेही";
+    document.getElementById("detail-2").innerHTML = "हे आपल्याला एकाच ठिकाणी आपले सर्व तपशील व्यवस्थित आणि व्यवस्थापित करण्यात मदत करते. यात प्रवेश करणे सोपे आहे आणि आपण या समर्पित अनुप्रयोगासह जाता जाता आपल्या ऑर्डरचा मागोवा घेऊ शकता.";
+    document.getElementById("detail-3").innerHTML = "आजकाल कोविड साथीच्या परिस्थितीनुसार, सर्वोत्तम लॉजिस्टिक्स सेवा पुरवठादार शोधणे खूप अवघड आहे, लॉजियो तुमच्यासाठी या समस्या सोडवते.";
+    document.getElementById("detail-4").innerHTML = "आपण सर्वोत्तम रसद सेवा शोधत असाल तर लॉजियो आपला मित्र आहे, खरंच!"
 }

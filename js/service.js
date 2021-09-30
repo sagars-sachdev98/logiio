fd51ee4847c26a0ac6d149ed4775f5a186002799
rahulsharma09 <08rahul.sharma01@gmail.com>
2021-09-30 06:57:43 +0530

    update

--- js/service.js
+++ js/service.js
@@ -2,22 +2,50 @@ function langaugeEnglish() {
   document.getElementById("solve-title").innerHTML = "What does Logiio Solve?";
   document.getElementById("loggio-p1").innerHTML =
     "Logiio is a mobile application that is designed especially for you to bring unique solutions and fulfillment of the orders, assuring great quality, speed, and reasonable cost. It has been built with keeping end-to-end logistics in mind and it aims to provide various unique solutions for all the right needs of the pickup and delivery of a courier that simplifies your life and makes it easier for on-the-go orders and delivery of parcels.";
-  document.getElementById("loggio-p2").innerHTML = "Logiio combines people, processes, and technology into one system to get the best results from all sources. The ultimate objective is to get maximum value from every division within the business while providing the highest customer satisfaction.";
-  document.getElementById("loggio-p3").innerHTML = "Logiio saves your time by reducing the paperwork and saving order-related details. It also helps for live tracking of orders.";
+  document.getElementById("loggio-p2").innerHTML =
+    "Logiio combines people, processes, and technology into one system to get the best results from all sources. The ultimate objective is to get maximum value from every division within the business while providing the highest customer satisfaction.";
+  document.getElementById("loggio-p3").innerHTML =
+    "Logiio saves your time by reducing the paperwork and saving order-related details. It also helps for live tracking of orders.";
+
+  //   What does logiio do?
+  document.getElementById("do-title").innerHTML = "What does Logiio do?";
+  document.getElementById("do-p1").innerHTML =
+    "Logiio provides end-to-end delivery services. It helps to save time and hassle that comes with the parcel delivery of logistics. It has a tracking system that monitors the whereabouts of each parcel at all times according to your preferences, including orders and other related details. There is also an order management system for users who would like to track their orders.";
+  document.getElementById("do-p2").innerHTML =
+    "Logiio helps in providing the best service by organizing and managing the whole unorganized sector of logistics. You just have to enter the weight, source, and destination of the parcel and it will automatically calculate the cost for your service.";
 }
 
 function languageHindi() {
-  document.getElementById("solve-title").innerHTML = "लोगियो क्या हल करता है?";
+  // What does logiio solves?
+  document.getElementById("solve-title").innerHTML = "लॉजियो क्या हल करता है?";
   document.getElementById("loggio-p1").innerHTML =
-    "लोगियो एक मोबाइल एप्लिकेशन है जो विशेष रूप से आपके लिए अद्वितीय समाधान लाने के लिए डिज़ाइन किया गया है और आदेशों की पूर्ति, महान गुणवत्ता, गति और उचित लागत का आश्वासन।  यह बनाया गया हैएंड-टू-एंड लॉजिस्टिक्स को ध्यान में रखते हुए और इसका उद्देश्य सभी के लिए विभिन्न अद्वितीय समाधान प्रदान करना है पिकअप और एक कूरियर की डिलीवरी की सही जरूरतें जो आपके जीवन को सरल बनाती हैं और इसे बनाती हैं ऑन-द-गो ऑर्डर और पार्सल की डिलीवरी के लिए आसान है।";
-  document.getElementById("loggio-p2").innerHTML = "लोगियो सभी स्रोतों से सबसे अच्छा परिणाम प्राप्त करने के लिए लोगों, प्रक्रियाओं और प्रौद्योगिकी को एक प्रणाली में जोड़ती है। अंतिम उद्देश्य उच्चतम ग्राहक संतुष्टि प्रदान करते हुए व्यवसाय के भीतर हर डिवीजन से अधिकतम मूल्य प्राप्त करना है।"
-  document.getElementById("loggio-p3").innerHTML = "लोगियो कागजी कार्रवाई को कम करने और आदेश से संबंधित विवरण की बचत करके अपना समय बचाता है । यह ऑर्डर की लाइव ट्रैकिंग के लिए भी मदद करता है।";
+    "लॉजियो एक मोबाइल एप्लिकेशन है जो विशेष रूप से आपके लिए अद्वितीय समाधान लाने के लिए डिज़ाइन किया गया है और आदेशों की पूर्ति, महान गुणवत्ता, गति और उचित लागत का आश्वासन।  यह बनाया गया हैएंड-टू-एंड लॉजिस्टिक्स को ध्यान में रखते हुए और इसका उद्देश्य सभी के लिए विभिन्न अद्वितीय समाधान प्रदान करना है पिकअप और एक कूरियर की डिलीवरी की सही जरूरतें जो आपके जीवन को सरल बनाती हैं और इसे बनाती हैं ऑन-द-गो ऑर्डर और पार्सल की डिलीवरी के लिए आसान है।";
+  document.getElementById("loggio-p2").innerHTML =
+    "लॉजियो सभी स्रोतों से सबसे अच्छा परिणाम प्राप्त करने के लिए लोगों, प्रक्रियाओं और प्रौद्योगिकी को एक प्रणाली में जोड़ती है। अंतिम उद्देश्य उच्चतम ग्राहक संतुष्टि प्रदान करते हुए व्यवसाय के भीतर हर डिवीजन से अधिकतम मूल्य प्राप्त करना है।";
+  document.getElementById("loggio-p3").innerHTML =
+    "लॉजियो कागजी कार्रवाई को कम करने और आदेश से संबंधित विवरण की बचत करके अपना समय बचाता है । यह ऑर्डर की लाइव ट्रैकिंग के लिए भी मदद करता है।";
+
+  //   What does logiio do?
+  document.getElementById("do-title").innerHTML = "लॉजियो क्या करता है? ";
+  document.getElementById("do-p1").innerHTML =
+    "लॉजियो एंड-टू-एंड डिलीवरी सेवाएं प्रदान करता है। यह आने वाले समय और परेशानी को बचाने में मदद करता है।इसमें एक ट्रैकिंग सिस्टम है जो प्रत्येक के ठिकाने पर नज़र रखता है  ऑर्डर और अन्य संबंधित विवरणों सहित आपकी प्राथमिकताओं के अनुसार हर समय पार्सल। उन उपयोगकर्ताओं के लिए एक आदेश प्रबंधन प्रणाली भी है जो अपने आदेशों को ट्रैक करना चाहते हैं।";
+  document.getElementById("do-p2").innerHTML =
+    "लॉजियो रसद के पूरे असंगठित क्षेत्र को व्यवस्थित और प्रबंधित करके सर्वोत्तम सेवा प्रदान करने में मदद करता है। आपको बस पार्सल का वजन, स्रोत और गंतव्य दर्ज करना होगा और यह स्वचालित रूप से आपकी सेवा की लागत की गणना करेगा।";
 }
 
 function languageMarathi() {
   document.getElementById("solve-title").innerHTML = "लॉगिओ काय सोडवतो?";
   document.getElementById("loggio-p1").innerHTML =
     "लॉगिओ हे एक मोबाइल अनुप्रयोग आहे जे विशेषत: आपल्यासाठी अद्वितीय उपाय आणण्यासाठी डिझाइन केले गेले आहे आण आदेशांची पूर्तता, उत्तम गुणवत्ता, वेग आणि वाजवी किंमत याची खात्री.  ते बांधले गेले आहे एंड-टू-एंड लॉजिस्टिक्स लक्षात ठेवून आणि सर्वांसाठी विविध अद्वितीय उपाय प्रदान करण्याचे उद्दीष्ट आहे पिकअप आणि डिलिव्हरीच्या योग्य गरजा. कुरिअरचे जे आपले जीवन सोपे करते आणि बनवते ऑन-द-गो ऑर्डर्स आणि पार्सल वितरित करणे सोपे आहे.";
-  document.getElementById("loggio-p2").innerHTML = "सर्व स्त्रोतांकडून सर्वोत्तम परिणाम मिळविण्यासाठी लोगिओ लोक, प्रक्रिया आणि तंत्रज्ञान यांना एका प्रणालीत एकत्र करते. ग्राहकांचे सर्वोच्च समाधान प्रदान करताना व्यवसायातील प्रत्येक विभागातून जास्तीत जास्त मूल्य मिळवणे हे अंतिम उद्दीष्ट आहे."
-  document.getElementById("loggio-p3").innerHTML = "लॉगिओ कागदोपत्री काम कमी करून आणि ऑर्डरशी संबंधित तपशील वाचवून आपला वेळ वाचवतो. हे ऑर्डरचा थेट मागोवा घेण्यासाठी देखील मदत करते.";
+  document.getElementById("loggio-p2").innerHTML =
+    "सर्व स्त्रोतांकडून सर्वोत्तम परिणाम मिळविण्यासाठी लोगिओ लोक, प्रक्रिया आणि तंत्रज्ञान यांना एका प्रणालीत एकत्र करते. ग्राहकांचे सर्वोच्च समाधान प्रदान करताना व्यवसायातील प्रत्येक विभागातून जास्तीत जास्त मूल्य मिळवणे हे अंतिम उद्दीष्ट आहे.";
+  document.getElementById("loggio-p3").innerHTML =
+    "लॉगिओ कागदोपत्री काम कमी करून आणि ऑर्डरशी संबंधित तपशील वाचवून आपला वेळ वाचवतो. हे ऑर्डरचा थेट मागोवा घेण्यासाठी देखील मदत करते.";
+
+  //   What does logiio do?
+  document.getElementById("do-title").innerHTML = "लॉजियो काय करते? ";
+  document.getElementById("do-p1").innerHTML =
+    "लॉजियो एंड-टू-एंड डिलीव्हरी सेवा प्रदान करते. हे वेळ वाचवण्यास मदत करते आणि रसद वितरणाची अडचण. यात एक ट्रॅकिंग सिस्टम आहे जी प्रत्येकाच्या ठावठिकाणी निरीक्षण करते ऑर्डर आणि इतर संबंधित तपशीलांसह, आपल्या आवडीनुसार प्रत्येक वेळी पार्सल करा. वापरकर्त्यांसाठी ऑर्डर व्यवस्थापन प्रणाली देखील आहे जी त्यांच्या ऑर्डरचा मागोवा घेऊ इच्छित आहेत. ";
+  document.getElementById("do-p2").innerHTML =
+    "लॉजियो संपूर्ण असंघटित संघटित आणि व्यवस्थापित करून सर्वोत्तम सेवा प्रदान करण्यात मदत करते रसद क्षेत्र. आपल्याला फक्त पार्सलचे वजन, स्त्रोत आणि गंतव्य प्रविष्ट करावे लागेल तुमच्या सेवेच्या खर्चाची आपोआप गणना होईल.";
 }
